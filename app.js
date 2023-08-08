a38994084333e515c2c293e4da07f9929aa399ab
Ricardo Zepeda <rzlazo@gmail.com>
2023-08-07 20:01:53 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+// Enable CORS for all routes
+app.use(cors({
+    origin: 'http://localhost:5173' // Replace with your SvelteKit frontend URL
+  }));
+
+// Create Socket.io instance with CORS options
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173", // Your SvelteKit frontend URL
+      methods: ["GET", "POST"]
+    }
+  });
+// Example GET endpoint (existing)
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// POST endpoint to receive data and emit to Socket.io
+app.post('/api/update', (req, res) => {
+    const data = req.body;
+  
+    try {
+      const { speckleUrl, passports } = data; // Changed from 'body' to 'data'
+  
+      if (speckleUrl && passports) {
+        // Emit the event to connected clients
+        io.emit('dataUpdated', data); // Assuming 'io' is your Socket.io server instance
+  
+        res.json({ message: speckleUrl });
+      } else {
+        res.status(400).json({ error: 'bad passportid or speckle url' });
+      }
+    } catch (error) {
+      console.error('Error processing request:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
